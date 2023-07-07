c239f5a604f4812bbd4b97cc879329e7327a767c
Maxim <334890@gmail.com>
2023-07-07 14:45:02 +0600

    add ships to gameboard

--- gameboard.js
+++ gameboard.js
@@ -1,4 +1,18 @@
 import { squareNameToCoordinates, coordinatesToSquareName } from "./utilities";
+import { Ship } from "./ship";
+
+const defaultShips = [
+  ["a1", "a1"],
+  ["a3", "a3"],
+  ["a5", "a5"],
+  ["a7", "a7"],
+  ["a9", "a10"],
+  ["c9", "c10"],
+  ["d9", "d10"],
+  ["c1", "c3"],
+  ["e1", "e3"],
+  ["j1", "j4"],
+];
 
 export function Gameboard() {
   const ships = [];
@@ -9,7 +23,16 @@ export function Gameboard() {
   }
 
   function isAllSunk() {
-    //
+    if (ships.length === 0) {
+      return undefined;
+    }
+    return ships.reduce((verdict, currentShip) => {
+      return verdict && currentShip.isSunk;
+    }, true);
+  }
+
+  function addShips(coordinates) {
+    coordinates.forEach((item) => ships.push(Ship(item)));
   }
 
   return {
@@ -17,5 +40,6 @@ export function Gameboard() {
     hits,
     receiveAttack,
     isAllSunk,
+    addShips,
   };
 }
---
+++ gameboard.test.js
@@ -0,0 +1,49 @@
+const { Gameboard } = require("./gameboard.js");
+
+const defaultShips = [
+  ["a1", "a1"],
+  ["a3", "a3"],
+  ["a5", "a5"],
+  ["a7", "a7"],
+  ["a9", "a10"],
+  ["c9", "c10"],
+  ["d9", "d10"],
+  ["c1", "c3"],
+  ["e1", "e3"],
+  ["j1", "j4"],
+];
+
+test("Gameboard ship placement", () => {
+  const newBoard = Gameboard();
+  expect(newBoard.ships.length).toBe(0);
+  newBoard.addShips(defaultShips);
+  expect(newBoard.ships.length).toBe(10);
+});
+
+test("Gameboard receive attacks", () => {
+  const newBoard = Gameboard();
+  expect(newBoard.hits[0]).toBe(undefined);
+  newBoard.receiveAttack("a1");
+  expect(newBoard.hits[0]).toBe("a1");
+  expect(newBoard.hits[1]).toBe(undefined);
+});
+
+test("Gameboard detect defeat", () => {
+  const newBoard = Gameboard();
+  expect(newBoard.isAllSunk()).toBe(undefined);
+  newBoard.addShips(defaultShips);
+  expect(newBoard.isAllSunk()).toBe(false);
+  newBoard.ships.forEach((ship) => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+  });
+  expect(newBoard.isAllSunk()).toBe(false);
+  newBoard.ships.forEach((ship) => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+  });
+  expect(newBoard.isAllSunk()).toBe(true);
+});
