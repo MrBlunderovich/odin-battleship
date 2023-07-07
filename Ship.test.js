6a2040d8dc23a3e5e351453e1c957e55f9ba69f4
Maxim <334890@gmail.com>
2023-07-07 12:04:20 +0600

    implement Ship squares output

--- Ship.test.js
+++ Ship.test.js
@@ -1,13 +1,26 @@
-const { Ship } = require("./index.js");
+const {
+  Ship,
+  squareNameToCoordinates,
+  coordinatesToSquareName,
+} = require("./index.js");
 
-test("Ship FF returns an object we expect", () => {
-  const newShip = Ship(3);
-  expect(newShip.shipLength).toBe(3);
-  expect(newShip.isSunk).toBe(false);
+test("Ship refuses incorrect coordinates", () => {
+  expect(() => Ship(["a1", "a10"])).toThrow();
+  expect(() => Ship(["a1", "f1"])).toThrow();
+  expect(() => Ship(["a1", "b3"])).toThrow();
+  expect(() => Ship(["a0", "a1"])).toThrow();
 });
 
-test("Ship sinks properly", () => {
-  const newShip = Ship(3);
+test("Ship exposes occupied squares", () => {
+  const newShip = Ship(["a1", "a3"]);
+  expect(newShip.squares[0]).toBe("a1");
+  expect(newShip.squares[1]).toBe("a2");
+  expect(newShip.squares[2]).toBe("a3");
+  expect(newShip.squares[3]).toBe(undefined);
+});
+
+test("Ship gets hits and sinks properly", () => {
+  const newShip = Ship(["a1", "a3"]);
   expect(newShip.isSunk).toBe(false);
   newShip.hit();
   expect(newShip.isSunk).toBe(false);
@@ -15,4 +28,12 @@ test("Ship sinks properly", () => {
   expect(newShip.isSunk).toBe(false);
   newShip.hit();
   expect(newShip.isSunk).toBe(true);
+});
+
+test("Square name and coordinates conversion", () => {
+  expect(squareNameToCoordinates("j10")[0]).toBe(9);
+  expect(squareNameToCoordinates("j10")[1]).toBe(9);
+  expect(squareNameToCoordinates(coordinatesToSquareName([1, 9]))[0]).toBe(1);
+  expect(squareNameToCoordinates(coordinatesToSquareName([1, 9]))[1]).toBe(9);
+  expect(coordinatesToSquareName(squareNameToCoordinates("d3"))[0]).toBe("d");
 });
--- index.js
+++ index.js
@@ -1,5 +1,56 @@
-export function Ship(length) {
-  const shipLength = length;
+export function Ship([bow, stern]) {
+  const coordinates = [
+    ...squareNameToCoordinates(bow),
+    ...squareNameToCoordinates(stern),
+  ];
+  let shipLength = null;
+  let shipIsVertical = null;
+  if (coordinates[0] === coordinates[2]) {
+    shipIsVertical = true;
+    shipLength = checkLength(coordinates[1], coordinates[3]);
+  } else if (coordinates[1] === coordinates[3]) {
+    shipIsVertical = false;
+    shipLength = checkLength(coordinates[0], coordinates[2]);
+  } else {
+    throw new Error("invalid coordinates");
+  }
+
+  function checkLength(start, end) {
+    const length = Math.abs(start - end) + 1;
+    if (length > 4) {
+      throw new Error("too long a ship");
+    }
+    return length;
+  }
+
+  const squares = shipSquares();
+
+  function shipSquares() {
+    if (shipLength === 1) {
+      return [bow];
+    }
+    if (shipLength === 2) {
+      return [bow, stern];
+    }
+    const squares = [];
+    if (shipIsVertical) {
+      const x = coordinates[0];
+      const yStart = Math.min(coordinates[1], coordinates[3]);
+      const yEnd = Math.max(coordinates[1], coordinates[3]);
+      for (let y = yStart; y <= yEnd; y++) {
+        squares.push(coordinatesToSquareName([x, y]));
+      }
+    } else {
+      const y = coordinates[1];
+      const xStart = Math.min(coordinates[0], coordinates[2]);
+      const xEnd = Math.max(coordinates[0], coordinates[2]);
+      for (let x = xStart; x <= xEnd; x++) {
+        squares.push(coordinatesToSquareName([x, y]));
+      }
+    }
+    return squares;
+  }
+
   let timesHit = 0;
 
   function hit() {
@@ -7,10 +58,47 @@ export function Ship(length) {
   }
 
   return {
-    shipLength,
     get isSunk() {
       return timesHit >= shipLength;
     },
     hit,
+    squares,
   };
+}
+//////////////////////////////////////////////////////////////
+export function Gameboard() {
+  const ships = [];
+  const hits = [];
+
+  function receiveAttack(squareName) {
+    hits.push(squareName);
+  }
+
+  function isAllSunk() {
+    //
+  }
+
+  return {
+    ships,
+    hits,
+    receiveAttack,
+    isAllSunk,
+  };
+}
+
+export function squareNameToCoordinates(square) {
+  const letter = square.slice(0, 1);
+  const x = letter.charCodeAt() - 97;
+  const y = +square.slice(1) - 1;
+  if (x < 0 || y < 0 || x > 9 || y > 9) {
+    throw new Error("invalid input");
+  }
+  return [x, y];
+}
+
+export function coordinatesToSquareName([x, y]) {
+  if (x < 0 || y < 0 || x > 9 || y > 9) {
+    throw new Error("invalid input");
+  }
+  return String.fromCharCode(x + 97) + (y + 1);
 }
